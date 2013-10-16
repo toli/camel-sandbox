b1fd2b97efd9f952cf9e4c533ffe30d0cc8ca937
James Carman <jcarman@apache.org>
2013-10-16 09:30:08 -0400

    Creating file example.

---
+++ src/test/java/com/carmanconsulting/sandbox/camel/FileTest.java
@@ -0,0 +1,35 @@
+package com.carmanconsulting.sandbox.camel;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.util.FileUtil;
+import org.junit.Test;
+
+import java.io.File;
+
+public class FileTest extends JmsTestCase
+{
+    @Test
+    public void testFileUnchanged() throws Exception
+    {
+        FileUtil.deleteFile(new File("target/outbox/order.xls"));
+        FileUtil.copyFile(new File("./target/test-classes/order.xls"), new File("./target/inbox/order.xls"));
+        Thread.sleep(1000);
+        assertFileExists("target/outbox/order.xls");
+
+
+    }
+
+    @Override
+    protected RouteBuilder createRouteBuilder() throws Exception
+    {
+        return new RouteBuilder()
+        {
+            @Override
+            public void configure() throws Exception
+            {
+                from("file://target/inbox").to("jms:queue:myfiles");
+                from("jms:queue:myfiles").to("file://target/outbox");
+            }
+        };
+    }
+}
