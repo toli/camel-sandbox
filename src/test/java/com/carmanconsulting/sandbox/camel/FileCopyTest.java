f3de7a2a46d585847626c6437b53ad8d33d7dffb
James Carman <jcarman@apache.org>
2013-10-16 11:33:59 -0400

    Added test using direct endpoints also.

--- src/test/java/com/carmanconsulting/sandbox/camel/FileCopyTest.java
+++ src/test/java/com/carmanconsulting/sandbox/camel/FileCopyTest.java
@@ -2,19 +2,36 @@ package com.carmanconsulting.sandbox.camel;
 
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.util.FileUtil;
+import org.apache.commons.io.IOUtils;
 import org.junit.Test;
 
 import java.io.File;
+import java.io.FileInputStream;
 
 public class FileCopyTest extends JmsTestCase
 {
     @Test
     public void testFileUnchangedThroughJms() throws Exception
     {
-        FileUtil.deleteFile(new File("target/outbox/order.xls"));
-        FileUtil.copyFile(new File("./target/test-classes/order.xls"), new File("./target/inbox/order.xls"));
-        Thread.sleep(1000);
-        assertFileExists("target/outbox/order.xls");
+        FileUtil.deleteFile(new File("target/outbox-jms/order.xls"));
+        FileUtil.copyFile(new File("target/test-classes/order.xls"), new File("target/inbox-jms/order.xls"));
+        Thread.sleep(2000);
+        assertFileExists("target/outbox-jms/order.xls");
+        byte[] original = IOUtils.toByteArray(new FileInputStream("target/test-classes/order.xls"));
+        byte[] copy = IOUtils.toByteArray(new FileInputStream("target/outbox-jms/order.xls"));
+        assertArrayEquals(original, copy);
+    }
+
+    @Test
+    public void testFileUnchangedThroughDirect() throws Exception
+    {
+        FileUtil.deleteFile(new File("target/outbox-direct/order.xls"));
+        FileUtil.copyFile(new File("target/test-classes/order.xls"), new File("target/inbox-direct/order.xls"));
+        Thread.sleep(2000);
+        assertFileExists("target/outbox-direct/order.xls");
+        byte[] original = IOUtils.toByteArray(new FileInputStream("target/test-classes/order.xls"));
+        byte[] copy = IOUtils.toByteArray(new FileInputStream("target/outbox-direct/order.xls"));
+        assertArrayEquals(original, copy);
     }
 
     @Override
@@ -25,8 +42,12 @@ public class FileCopyTest extends JmsTestCase
             @Override
             public void configure() throws Exception
             {
-                from("file://target/inbox").to("jms:queue:myfiles");
-                from("jms:queue:myfiles").to("file://target/outbox");
+                from("file://target/inbox-jms").to("jms:queue:myfiles");
+                from("jms:queue:myfiles").to("file://target/outbox-jms");
+
+                from("file://target/inbox-direct").to("direct:myfiles");
+                from("direct:myfiles").to("file://target/outbox-direct");
+
             }
         };
     }
