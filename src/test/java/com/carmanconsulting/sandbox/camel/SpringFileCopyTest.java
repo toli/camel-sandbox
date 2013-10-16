32a65061eaa672d36f175f68aa5e4f2821453d1f
James Carman <jcarman@apache.org>
2013-10-16 11:38:25 -0400

    Adding spring-based file copy test.

---
+++ src/test/java/com/carmanconsulting/sandbox/camel/SpringFileCopyTest.java
@@ -0,0 +1,32 @@
+package com.carmanconsulting.sandbox.camel;
+
+import org.apache.camel.test.spring.CamelSpringTestSupport;
+import org.apache.camel.util.FileUtil;
+import org.apache.commons.io.IOUtils;
+import org.apache.xbean.spring.context.ClassPathXmlApplicationContext;
+import org.junit.Test;
+import org.springframework.context.support.AbstractApplicationContext;
+
+import java.io.File;
+import java.io.FileInputStream;
+
+public class SpringFileCopyTest extends CamelSpringTestSupport
+{
+    @Override
+    protected AbstractApplicationContext createApplicationContext()
+    {
+        return new ClassPathXmlApplicationContext("classpath:file-copy.xml");
+    }
+
+    @Test
+    public void testFileUnchangedThroughDirect() throws Exception
+    {
+        FileUtil.deleteFile(new File("target/outbox-direct/order.xls"));
+        FileUtil.copyFile(new File("target/test-classes/order.xls"), new File("target/inbox-direct/order.xls"));
+        Thread.sleep(2000);
+        assertFileExists("target/outbox-direct/order.xls");
+        byte[] original = IOUtils.toByteArray(new FileInputStream("target/test-classes/order.xls"));
+        byte[] copy = IOUtils.toByteArray(new FileInputStream("target/outbox-direct/order.xls"));
+        assertArrayEquals(original, copy);
+    }
+}
